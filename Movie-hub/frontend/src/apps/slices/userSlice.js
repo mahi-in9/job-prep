e129ba4d570b7e08f9dd24c20e0fd5d4a8a47342
Mahendra Kumar <mahithegreat19@gmail.com>
2026-03-13 22:27:59 +0530

    push code

---
+++ Movie-hub/frontend/src/apps/slices/userSlice.js
@@ -0,0 +1,69 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const registerUser = createAsyncThunk(
+  "user/registerUser",
+  async (userData, thunkAPI) => {
+    try {
+      const res = await axios.post("/api/user/register", userData);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  },
+);
+
+export const loginUser = createAsyncThunk(
+  "user/loginUser",
+  async (userData, thunkAPI) => {
+    try {
+      const res = await axios.post("/api/user/login", userData);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  },
+);
+
+const initialState = {
+  user: null,
+  loading: false,
+  error: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.user = action.payload;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+
+      .addCase(loginUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.user = action.payload;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export default userSlice.reducer;
--- Movie-hub/frontend/src/apps/store.js
+++ Movie-hub/frontend/src/apps/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import movieReducer from "./slices/movieSlice";
+import userReducer from "./slices/userSlice";
 
 export const store = configureStore({
   reducer: {
+    user: userReducer,
     movies: movieReducer,
   },
 });
